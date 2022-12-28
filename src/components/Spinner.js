2133df599a940c3a1cbdc68954f12c0f2d003ccb
Taranjeet Singh Dhingra <taranjeetsingh257@gmail.com>
2022-12-27 19:24:02 -0500

    margin

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 export default class Spinner extends Component {
   render() {
     return (
-        <div className="text-center">
+        <div className="text-center my-3">
             <div className="spinner-border" role="status">
                 {/* <span class="sr-only">Loading...</span> */}
             </div>
